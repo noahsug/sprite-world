21cc07746f867e2b87e4867c66a17f5fa3c71593
sugarman <sugarman@google.com>
2016-11-04 17:47:38 -0700

    update keybindings

--- src/sagas/keyboard-input.js
+++ src/sagas/keyboard-input.js
@@ -4,7 +4,7 @@ import { awaitKeyDown } from './keyboard'
 
 export default function* keyboardInput() {
   while (true) {
-    const { key } = yield call(awaitKeyDown, /[1-2]/)
-    yield putp('USE_ABILITY', Number(key) - 1)
+    const { key } = yield call(awaitKeyDown, /[8-9]/)
+    yield putp('USE_ABILITY', Number(key) - 8)
   }
 }
